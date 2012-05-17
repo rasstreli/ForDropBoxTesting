111a644096603ea55c72b7c5ba1918a763c80fea
pavel <pavel@pavel-H61M-DS2.(none)>
2012-05-17 11:31:18 +0300

    test

--- alphaUp/server/connect.py
+++ alphaUp/server/connect.py
@@ -1,21 +1,9 @@
 __author__ = 'pavel'
-import mechanize
 import urllib2
-from dropbox import clients, session
-from settings import ACCESS_TYPE, APP_KEY, APP_SECRET
+from alphaUp.dropbox import client, session
+from alphaUp.settings import ACCESS_TYPE, APP_KEY, APP_SECRET
 
 sess = session.DropboxSession(APP_KEY,APP_SECRET,ACCESS_TYPE)
-class Connection(object):
-    email = ""
-    password = ""
-    root_ns = ""
-    token = ""
-    browser = None
-
-    def __init__(self,email,password):
-        self.email = email
-        self.password = password
-
-    def login(self):
-
+def login(url_authorize):
+    
 
